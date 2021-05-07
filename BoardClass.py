ab1212270f087fbaaeee4e4724bf35d273b480c3
Erik <Erik>
2021-05-07 19:01:54 +0400

    updated figure classes

--- BoardClass.py
+++ BoardClass.py
@@ -25,37 +25,37 @@ class Board:
 			figure = Soldier
 
 		# placing Kings
-		Self.board[0][4], self.board[-1][3] = King, King
+		self.board[0][4], self.board[-1][3] = King("black"), King("white")
 
 		# placing Queens
-		Self.board[0][4], self.board[-1][3] = Queen, Queen
+		self.board[0][4], self.board[-1][3] = Queen("black"), Queen("white")
 
 		# placing Bishops
-		Self.board[0][2], self.board[0][-3] = Bishop, Bishop
-		Self.board[-1][2], self.board[-1][-3] = Bishop, Bishop
+		self.board[0][2], self.board[0][-3] = Bishop(), Bishop()
+		self.board[-1][2], self.board[-1][-3] = Bishop(), Bishop()
 
 		# placing Horses
-		Self.board[0][1], self.board[0][-2] = Horse, Horse
-		Self.board[-1][1], self.board[-1][-2] = Horse, Horse
+		self.board[0][1], self.board[0][-2] = Horse(), Horse()
+		self.board[-1][1], self.board[-1][-2] = Horse(), Horse()
 
 		# placing Towers
-		Self.board[0][0], self.board[0][-1] = Tower, Tower
-		Self.board[-1][0], self.board[-1][-1] = Tower, Tower
+		self.board[0][0], self.board[0][-1] = Tower(), Tower()
+		self.board[-1][0], self.board[-1][-1] = Tower(), Tower()
 
 
-	# giving colors to figures(objects) on board
-	def board_initializing(self):
-		# initializing blue(black) figures
-		for figures in self.board[0]:
-			figures = f"\033[1;34;40m {Figures}"
-		for figures in self.board[1]:
-			figures = f"\033[1;34;40m {Figures}"
+	# # giving colors to figures(objects) on board
+	# def board_initializing(self):
+	# 	# initializing blue(black) figures
+	# 	for figures in self.board[0]:
+	# 		figures.color = f"\033[1;34;40m {figures}"
+	# 	for figures in self.board[1]:
+	# 		figures = f"\033[1;34;40m {figures}"
 
-		# initializing white figures
-		for figures in self.board[7]:
-			figures = f"\033[1;37;40m {Figures}"
-		for figures in self.board[6]:
-			figures = f"\033[1;37;40m {Figures}"
+	# 	# initializing white figures
+	# 	for figures in self.board[7]:
+	# 		figures = f"\033[1;37;40m {Figures}"
+	# 	for figures in self.board[6]:
+	# 		figures = f"\033[1;37;40m {Figures}"
 
 
 
@@ -71,11 +71,15 @@ class Board:
 
 				# simple move for figure in case if the go to cell is empty 
 				if self.board[self.figure_to[0]][self.figure_to[1]] == self.board[cells[0]][cells[1]] and self.board[cells[0]][cells[1]] == None:
+					
 					# moving the figure to given position 
 					self.board[self.figure_to[0]][self.figure_to[1]] = self.board[self.figure_from[0]][self.figure_from[1]]
+					
 					# making the inital coordinates None as figure moved from there
 					self.board[self.figure_from[0]][self.figure_from[1]] = None
+					
 					# clearing the possible_moves list for figure
 					self.board[self.figure_to[0]][self.figure_to[1]].poss_moves = []
+					
 					# clearing the possible_attacks list for figure
 					self.board[self.figure_to[0]][self.figure_to[1]].poss_attacks = []
--- Figures.py
+++ Figures.py
@@ -1,37 +1,55 @@
 class Figure:
 
-	def __init__(self, board):
- 
-		self.board = board
+	def __init__(self, figure_color):
+ 		self.figure_color = figure_color
+	
 		# attribute for all possible moves on deck for figure
-		self.poss_moves = [] 
+		self.poss_moves = [[], [], [], [], [], [], ,[], []] 
 		# attribute for all possible attack cells for figure on deck
 		self.poss_attacks = []
 		# attribute for figures position
-		self.position = []
+		self.position = [[], []]
 	
 	
 class King(Figure):
 
-	def __init__(self, figure_color):
-		self.figure_color = figure_color
 
 	def possible_moves(self):
 
 		# forward, backwards, left and right moving
 		for count in range(1, 2):
+
 			# checking forward cells
 			if self.position[0] - count > -1:
-				self.poss_moves.append([self.position[0] - count, self.position[1]])
+				self.poss_moves[0].append([self.position[0] - count, self.position[1]])
+
 			# cheking backwards cells
 			if self.position[0] + count < 8:
-				self.poss_moves.append([self.position[0] + count, self.position[1]])
+				self.poss_moves[1].append([self.position[0] + count, self.position[1]])
+
 			# checking left cells
 			if self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0], self.position[1] - count])
+				self.poss_moves[2].append([self.position[0], self.position[1] - count])
+
 			# checking right cells
-			if position[1] + count < 8:
-				self.poss_moves.append([self.position[0], self.position[1] + count])
+			if self.position[1] + count < 8:
+				self.poss_moves[3].append([self.position[0], self.position[1] + count])
+
+			# diagonally up right cells
+			if self.position[0] - count > -1 and self.position[1] + count < 8:
+				self.poss_moves[4].append([self.position[0] - count, self.position[1] + count])
+
+			# diagonally down left cells
+			if self.position[0] + count < 8 and self.position[1] - count > -1:
+				self.poss_moves[5].append([self.position[0] + count, self.position[1] - count])
+
+			# diagonally up left cells
+			if self.position[0] - count > - 1 and self.position[1] - count > -1:
+				self.poss_moves[6].append([self.position[0] - count, self.position[1] - count])
+
+			# diagonally down right cells
+			if self.position[0] + count < 8 and self.position[1] + count < 8:
+				self.poss_moves[7].append([self.position[0] + count, self.position[1] + count])
 
 		# the moves are same as attack spots
 		self.poss_moves = self.poss_attacks
@@ -39,9 +57,7 @@ class King(Figure):
 
 class Queen(Figure):
 
-	def __init__(self, figure_color):
 
-		self.figure_color = figure_color
 
 	def possible_moves(self):
 
@@ -49,31 +65,31 @@ class Queen(Figure):
 		for count in range(1, 8):
 			# checking forward cells
 			if self.position[0] - count > -1:
-				self.poss_moves.append([self.position[0] - count, self.position[1]])
+				self.poss_moves[0].append([self.position[0] - count, self.position[1]])
 			# cheking backwards cells
 			if self.position[0] + count < 8:
-				self.poss_moves.append([self.position[0] + count, self.position[1]])
+				self.poss_moves[1].append([self.position[0] + count, self.position[1]])
 			# checking left cells
 			if self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0], self.position[1] - count])
+				self.poss_moves[2].append([self.position[0], self.position[1] - count])
 			# checking right cells
 			if position[1] + count < 8:
-				self.poss_moves.append([self.position[0], self.position[1] + count])
+				self.poss_moves[3].append([self.position[0], self.position[1] + count])
 
 		# diagonally left/right, up/down moves
 		for count in range(1, 8):
 			# diagonally up right cells
 			if self.position[0] - count > -1 and self.position[1] + count < 8:
-				self.poss_moves.append([self.position[0] - count, self.position[1] + count])
+				self.poss_moves[4].append([self.position[0] - count, self.position[1] + count])
 			# diagonally down left cells
 			if self.position[0] + count < 8 and self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0] + count, self.position[1] - count])
+				self.poss_moves[5].append([self.position[0] + count, self.position[1] - count])
 			# diagonally up left cells
 			if self.position[0] - count > - 1 and self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0] - count, self.position[1] - count])
+				self.poss_moves[6].append([self.position[0] - count, self.position[1] - count])
 			# diagonally down right cells
 			if self.position[0] + count < 8 and self.position[1] + count < 8:
-				self.poss_moves.append([self.position[0] + count, self.position[1] + count])
+				self.poss_moves[7].append([self.position[0] + count, self.position[1] + count])
 			
 		# possible moves are the same as possible attacks
 		self.poss_moves = self.poss_attacks
@@ -81,41 +97,35 @@ class Queen(Figure):
 
 class Soldier(Figure):
 
-	def __init__(self, figure_color):
-
-		self.figure_color = figure_color
 
 	def possible_moves(self):
 		# initial two steps forward from start
 		if self.position[0] == 6:
-			self.poss_moves.append([self.position[0] - 1, self.position[1]])
-			self.poss_moves.append([self.position[0] - 2, self.position[1]])
+			self.poss_moves[0].append([self.position[0] - 1, self.position[1]])
+			self.poss_moves[1].append([self.position[0] - 2, self.position[1]])
 		# forward move
-		elif self.position[0] < 7 and self.position[0] != 6:
-			self.poss_moves.append([self.position[0] - 1, self.position[1]])
+		elif 0 < self.position[0] < 7 and self.position[0] != 6:
+			self.poss_moves[2].append([self.position[0] - 1, self.position[1]])
 
 	def possible_soldier_attacks(self):
 
 		# if soldier is on the first column of the board:
 		if self.position[1] == 0:
 			# diagonal right forward
-			self.poss_attacks.append([self.position[0] - 1, self.position[1] + 1])
+			self.poss_attacks[0].append([self.position[0] - 1, self.position[1] + 1])
 		# if soldier is on the last column of the board
 		elif self.position[1] == 7:
 			# diagonal left forward
-			self.poss_attacks.append([self.position[0] - 1, self.position[1] - 1])
+			self.poss_attacks[1].append([self.position[0] - 1, self.position[1] - 1])
 		else:
 			# diagonal left forward
-			self.poss_attacks.append([self.position[0] - 1, self.position[1] - 1])
+			self.poss_attacks[0].append([self.position[0] - 1, self.position[1] - 1])
 			# diagonal right forward
-			self.poss_attacks.append([self.position[0] - 1, self.position[1] + 1])
+			self.poss_attacks[1].append([self.position[0] - 1, self.position[1] + 1])
 
 
 class Tower(Figure):
 
-	def __init__(self, figure_color):
-
-		self.figure_color = figure_color
 
 	def possible_moves(self):
 
@@ -123,16 +133,16 @@ class Tower(Figure):
 		for count in range(1, 8):
 			# checking forward cells
 			if self.position[0] - count > -1:
-				self.poss_moves.append([self.position[0] - count, self.position[1]])
+				self.poss_moves[0].append([self.position[0] - count, self.position[1]])
 			# cheking backwards cells
 			if self.position[0] + count < 8:
-				self.poss_moves.append([self.position[0] + count, self.position[1]])
+				self.poss_moves[1].append([self.position[0] + count, self.position[1]])
 			# checking left cells
 			if self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0], self.position[1] - count])
+				self.poss_moves[2].append([self.position[0], self.position[1] - count])
 			# checking right cells
 			if position[1] + count < 8:
-				self.poss_moves.append([self.position[0], self.position[1] + count])
+				self.poss_moves[3].append([self.position[0], self.position[1] + count])
 
 
 		self.poss_moves = self.poss_attacks
@@ -141,35 +151,32 @@ class Tower(Figure):
 
 class Horse(Figure):
 
-	def __init__(self, figure_color):
-
-		self.figure_color = figure_color
 
 	def possible_moves(self):
 
 		if self.position[0] - 2 > -1 and self.position[1] - 1 > - 1:
 		    # forward left move
-		    self.poss_moves.append([self.position[0] - 2, self.position[1] - 1])
+		    self.poss_moves[0].append([self.position[0] - 2, self.position[1] - 1])
 		    # left forward move 
-		    self.poss_moves.append([self.position[0] - 1, self.position[1] - 2])
+		    self.poss_moves[1].append([self.position[0] - 1, self.position[1] - 2])
 
 		if self.position[0] + 2 < 8 and self.position[1] + 1 < 8:
 		    # backwards right move
-		    self.poss_moves.append([self.position[0] + 2, self.position[1] + 1])
+		    self.poss_moves[2].append([self.position[0] + 2, self.position[1] + 1])
 		    # right backwards move
-		    self.poss_moves.append([self.position[0] + 1, self.position[1] + 2])
+		    self.poss_moves[3].append([self.position[0] + 1, self.position[1] + 2])
 
 		if self.position[0] - 2 > -1 and self.position[1] + 2 < 8:
 		    # forward right move
-		    self.poss_moves.append([self.position[0] - 2, self.position[1] + 1])
+		    self.poss_moves[4].append([self.position[0] - 2, self.position[1] + 1])
 		    # right forward move
-		    self.poss_moves.append([self.position[0] - 1, self.position[1] + 2])
+		    self.poss_moves[5].append([self.position[0] - 1, self.position[1] + 2])
 
 		if self.position[0] + 2 < 8 and self.position[1] - 2 > -1:
 		    # left backwards move
-		    self.poss_moves.append([self.position[0] + 1, self.position[1] - 2])
+		    self.poss_moves[6].append([self.position[0] + 1, self.position[1] - 2])
 		    # backwards left move
-		    self.poss_moves.append([self.position[0] + 2, self.position[1] - 1])
+		    self.poss_moves[7].append([self.position[0] + 2, self.position[1] - 1])
 
 
 		# the moves are same as attack spots
@@ -178,9 +185,6 @@ class Horse(Figure):
 
 class Bishop(Figure):
 
-	def __init__(self, figure_color):
-
-		self.figure_color = figure_color
 
 	def possible_moves(self):
 
@@ -188,16 +192,16 @@ class Bishop(Figure):
 		for count in range(1, 8):
 			# diagonally up right cells
 			if self.position[0] - count > -1 and self.position[1] + count < 8:
-				self.poss_moves.append([self.position[0] - count, self.position[1] + count])
+				self.poss_moves[1].append([self.position[0] - count, self.position[1] + count])
 			# diagonally down left cells
 			if self.position[0] + count < 8 and self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0] + count, self.position[1] - count])
+				self.poss_moves[2].append([self.position[0] + count, self.position[1] - count])
 			# diagonally up left cells
 			if self.position[0] - count > - 1 and self.position[1] - count > -1:
-				self.poss_moves.append([self.position[0] - count, self.position[1] - count])
+				self.poss_moves[3].append([self.position[0] - count, self.position[1] - count])
 			# diagonally down right cells
 			if self.position[0] + count < 8 and self.position[1] + count < 8:
-				self.poss_moves.append([self.position[0] + count, self.position[1] + count])
+				self.poss_moves[4].append([self.position[0] + count, self.position[1] + count])
 
 		# moves are the same as attacks
 		self.poss_moves = self.poss_attacks
